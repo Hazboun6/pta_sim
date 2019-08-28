4f619429c84043b7eae3f733f690852b9aac7d20
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-08-28 16:01:32 -0700

    Added sampler declaration

--- pta_sim/scripts/solar_wind_psr.py
+++ pta_sim/scripts/solar_wind_psr.py
@@ -108,7 +108,7 @@ for ii,mod in enumerate(sw_models):
     model_params.update({ii : ptas[ii].param_names})
 
 super_model = model_utils.HyperModel(ptas)
-super_model.setup_sampler(resume=True, outdir=args.outdir)
+sampler = super_model.setup_sampler(resume=True, outdir=args.outdir)
 with open(args.outdir+'/model_params.json' , 'w') as fout:
     json.dump(model_params,fout,sort_keys=True,indent=4,separators=(',', ': '))
 
