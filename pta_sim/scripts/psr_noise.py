9c8399f1d1ffa9d1e588ce0c8b8d403007813e71
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-11-19 16:04:40 -0800

    Change par/tim wildcards

--- pta_sim/scripts/psr_noise.py
+++ pta_sim/scripts/psr_noise.py
@@ -46,9 +46,9 @@ if args.pickle=='no_pickle':
         psr = Pulsar(t, f.model, ephem='DE436')
     else:
         if args.psr == 'J1713+0747':
-            parfile = glob.glob(args.pardir + args.psr + '*.gls.t2.par')[0]
+            parfile = glob.glob(args.pardir + args.psr + '*.t2.*par')[0]
         else:
-            parfile = glob.glob(args.pardir + args.psr + '*.gls.par')[0]
+            parfile = glob.glob(args.pardir + args.psr + '*.gls*par')[0]
 
         timfile = glob.glob(args.timdir + args.psr + '*.tim')[0]
         psr = Pulsar(parfile, timfile, ephem='DE436')
