5361959ea5495e7f534ed5d3ef0dc075da0e299d
Hazboun6 <jeffrey.hazboun@gmail.com>
2020-02-21 13:23:00 -0800

    Added comparison plot.

--- pta_sim/scripts/plot_noise_flowers.py
+++ pta_sim/scripts/plot_noise_flowers.py
@@ -11,7 +11,10 @@ import pta_sim.parse_sim as parse_sim
 args = parse_sim.arguments()
 
 rootdir = '/home/jeffrey.hazboun/nanograv/12p5yr_work/noise_model/'
-chaindir = rootdir + 'psr_chains/{0}/1st_round/'.format(args.psr)
+if args.psr != 'J1918-0642':
+    chaindir = rootdir + 'psr_chains/{0}/1st_round/'.format(args.psr)
+else:
+    chaindir = rootdir + 'psr_chains/{0}/1st_round_rf/'.format(args.psr)
 
 plotdir = rootdir + '1st_round_plots/'
 label = 'PSR {}, 1st Round Model Selection'.format(args.psr)
@@ -31,6 +34,16 @@ c0 = cH.model_core(n_most)
 dg.plot_chains(c0, hist=False, ncols=5, title_y=1.05,
                exclude=['lnprior', 'pt_chain_accept'],
                save=plotdir+'{0}_nmost_param_chains.pdf'.format(args.psr))
+
+std_dir = '/home/joseph.simon/12p5yr_GWB/V3_dataset/chains/'
+std_dir += 'SinglePsr_StandardNoise/{0}/singlepsrNoise/'.format(args.psr)
+std_core = co.Core(label='Std RN Plaw {0}'.format(args.psr),
+                   chaindir=std_dir)
+
+dg.plot_chains([c5,std_core], hist=True, pars=std_core.params[:-4], ncols=4,
+               title_y=1.05,linewidth=2,
+               legend_labels=['Best Model','Std DMX'],
+               plotdir+'{0}_std_noise_compare.pdf'.format(args.psr))
 
 with open(chaindir+'/model_labels.json' , 'r') as fin:
     model_labels= json.load(fin)
