a2f4e7b9e9ed4d281ad0bf2d14d91554f3c67224
Hazboun6 <jeffrey.hazboun@gmail.com>
2020-03-07 00:56:27 -0800

    Added different labels

--- pta_sim/scripts/ng12p5yr_nm_j1713_r2a.py
+++ pta_sim/scripts/ng12p5yr_nm_j1713_r2a.py
@@ -29,48 +29,6 @@ model_labels = [['A', 'periodic', True, True, True, True],
                 ['D', 'periodic', True, False, True, True],
                 ['E', 'periodic', False, True, True, True],
                 ]
-ptas = {}
-all_kwargs = {}
-for ii, ent in enumerate(model_labels):
-    num_dips = 0
-    dm_expdip_tmin = []
-    dm_expdip_tmax = []
-    dm_expdip_idx = []
-
-    if ent[2]:
-        num_dips +=1
-        dm_expdip_tmin.append(54700)
-        dm_expdip_tmax.append(54850)
-        dm_expdip_idx.append(2)
-    if ent[3]:
-        num_dips +=1
-        dm_expdip_tmin.append(57450)
-        dm_expdip_tmax.append(57450)
-        dm_expdip_idx.append(2)
-    if ent[4]:
-        num_dips +=1
-        dm_expdip_tmin.append(54700)
-        dm_expdip_tmax.append(54850)
-        dm_expdip_idx.append(4)
-    if ent[5]:
-        num_dips +=1
-        dm_expdip_tmin.append(57450)
-        dm_expdip_tmax.append(57450)
-        dm_expdip_idx.append(4)
-
-    dip_kwargs = {'dm_expdip':True,
-                  'dmexp_sign': 'negative',
-                  'num_dmdips':num_dips,
-                  'dm_expdip_idx':dm_expdip_idx,
-                  'dm_expdip_tmin':dm_expdip_tmin,
-                  'dm_expdip_tmax':dm_expdip_tmax,
-                  'dmdip_seqname':'ism'}
-
-    kwargs = copy.deepcopy(model_kwargs['5'])
-    kwargs.update(dip_kwargs)
-    ptas[ii] = model_singlepsr_noise(psr, **kwargs)
-    all_kwargs[ii] = kwargs
-
 
 # model_labels = [['A', 'periodic', True, True, False, False],
 #                 ['B', 'periodic', False, False, True, True],
@@ -78,6 +36,53 @@ for ii, ent in enumerate(model_labels):
 #                 ['D', 'periodic', False, True, True, False],
 #                 ['E', 'periodic', True, False, True, False],
 #                 ['F', 'periodic', False, True, False, True]]
+
+ptas = {}
+all_kwargs = {}
+for ii, ent in enumerate(model_labels):
+    num_dips = 0
+    dm_expdip_tmin = []
+    dm_expdip_tmax = []
+    dm_expdip_idx = []
+    dmdip_seqname = []
+
+    if ent[2]: # Add 1st ISM event, DM
+        num_dips +=1
+        dm_expdip_tmin.append(54700)
+        dm_expdip_tmax.append(54850)
+        dm_expdip_idx.append(2)
+        dmdip_seqname.append('dm_1')
+    if ent[3]: # Add 2nd ISM event, DM
+        num_dips +=1
+        dm_expdip_tmin.append(57450)
+        dm_expdip_tmax.append(57450)
+        dm_expdip_idx.append(2)
+        dmdip_seqname.append('dm_2')
+    if ent[4]: # Add 1st ISM event, Scattering
+        num_dips +=1
+        dm_expdip_tmin.append(54700)
+        dm_expdip_tmax.append(54850)
+        dm_expdip_idx.append(4)
+        dmdip_seqname.append('chrom_1')
+    if ent[5]: # Add 2nd ISM event, Scattering
+        num_dips +=1
+        dm_expdip_tmin.append(57450)
+        dm_expdip_tmax.append(57450)
+        dm_expdip_idx.append(4)
+        dmdip_seqname.append('chrom_2')
+
+    dip_kwargs = {'dm_expdip':True,
+                  'dmexp_sign': 'negative',
+                  'num_dmdips':num_dips,
+                  'dm_expdip_idx':dm_expdip_idx,
+                  'dm_expdip_tmin':dm_expdip_tmin,
+                  'dm_expdip_tmax':dm_expdip_tmax,
+                  'dmdip_seqname':dmdip_seqname}
+
+    kwargs = copy.deepcopy(model_kwargs['5'])
+    kwargs.update(dip_kwargs)
+    ptas[ii] = model_singlepsr_noise(psr, **kwargs)
+    all_kwargs[ii] = kwargs
 
 super_model = HyperModel(ptas)
 
@@ -93,7 +98,7 @@ with open(args.outdir + '/model_params.json', 'w') as fout:
               indent=4, separators=(',', ': '))
 
 with open(args.outdir + '/model_kwargs.json', 'w') as fout:
-    json.dump(model_kwargs, fout, sort_keys=True,
+    json.dump(all_kwargs, fout, sort_keys=True,
               indent=4, separators=(',', ': '))
 
 with open(args.outdir + '/model_labels.json', 'w') as fout:
