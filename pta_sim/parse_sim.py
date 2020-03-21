5376294b5a02feddce959ee37d82cd169cfd0730
Hazboun6 <jeffrey.hazboun@gmail.com>
2020-03-21 11:15:59 -0700

    Added missing quotes.

--- pta_sim/parse_sim.py
+++ pta_sim/parse_sim.py
@@ -126,11 +126,11 @@ parser.add_argument('--sky_scramble', dest='sky_scramble',
 parser.add_argument('--spat_corr_info', dest='spat_corr_info',
                     action='store_true', default=False,
                     help='Whether to write out the spatial correlation information')
-parser.add_argument('--sw_r2p', dest='sw_r2p', action=append,
+parser.add_argument('--sw_r2p', dest='sw_r2p', action='append',
                     type=lambda s: [float(item) for item in s.split(',')],
                     help='Power(s) of solar wind model to use. If list of two floats is given these will become a uniform prior.')
 parser.add_argument('--sw_r2p_ranges', dest='sw_r2p_ranges',
-                    action=append,
+                    action='append',
                     type=lambda s: [float(item) for item in s.split(',')],
                     help='Amplitude for extra terms in solar wind. Range for log uniform prior provided as two item list. ')
 parser.add_argument('--sw_r4p4', dest='sw_r4p4',
