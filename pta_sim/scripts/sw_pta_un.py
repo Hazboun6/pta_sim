ea16a211d08a6cb1153d6b03fd74aeb4cbbf06d4
Hazboun6 <jeffrey.hazboun@gmail.com>
2021-03-29 09:45:27 -0700

    fix import 2

--- pta_sim/scripts/sw_pta_un.py
+++ pta_sim/scripts/sw_pta_un.py
@@ -7,6 +7,7 @@ import numpy as np
 # import pint.residuals as r
 import astropy.units as u
 import scipy.integrate as spi
+import scipy.stats as sps
 import enterprise
 from enterprise.pulsar import Pulsar
 import enterprise.signals.parameter as parameter
