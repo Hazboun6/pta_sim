138eea007bad21a72c5ecff5fd2e2fa5ef67f0ae
Hazboun6 <jeffrey.hazboun@gmail.com>
2022-09-18 22:38:06 -0700

    adding script for time domain gp 9

--- pta_sim/scripts/ng15yr_time_domain_gp.py
+++ pta_sim/scripts/ng15yr_time_domain_gp.py
@@ -101,27 +101,29 @@ else:
     vla_log10_gam_p = parameter.Uniform(-2, 2)('vla_log10_gam_p')
 
     @signal_base. function
-    def linear_interp_basis_time(name, toas, dt=7*const.day):
-        # get linear interpolation basis in time
-        U, avetoas = utils.linear_interp_basis(toas, dt=dt)
+    def linear_interp_basis_msk(toas, flags, flagvals=[], dt=7*const.day):
+         # get linear interpolation basis in time
+         mask = [be in flagvals for be in flags['be']]
+         msk_toas = toas[mask]
+         U, avetoas = utils.linear_interp_basis(msk_toas, dt=dt)
 
-        return U, avetoas
+         return U, avetoas
 
-    dm_basis = linear_interp_basis_time(dt=7*const.day)
+    ao_basis = linear_interp_basis_time(flagvals=["ASP", "PUPPI"],dt=7*const.day)
     ao_qp = periodic_kernel(log10_sigma=ao_log10_sigma,
-                         log10_ell=ao_log10_ell,
-                         log10_gam_p=ao_log10_gam_p,
-                         log10_p=ao_log10_p)
-
+                            log10_ell=ao_log10_ell,
+                            log10_gam_p=ao_log10_gam_p,
+                            log10_p=ao_log10_p)
+    gbt_basis = linear_interp_basis_time(flagvals=["GASP", "GUPPI"],dt=7*const.day)
     gbt_qp = periodic_kernel(log10_sigma=gbt_log10_sigma,
-                         log10_ell=gbt_log10_ell,
-                         log10_gam_p=gbt_log10_gam_p,
-                         log10_p=gbt_log10_p)
-
+                             log10_ell=gbt_log10_ell,
+                             log10_gam_p=gbt_log10_gam_p,
+                             log10_p=gbt_log10_p)
+    vla_basis = linear_interp_basis_time(flagvals=["GASP", "GUPPI"],dt=7*const.day)
     vla_qp = periodic_kernel(log10_sigma=vla_log10_sigma,
-                         log10_ell=vla_log10_ell,
-                         log10_gam_p=vla_log10_gam_p,
-                         log10_p=vla_log10_p)
+                             log10_ell=vla_log10_ell,
+                             log10_gam_p=vla_log10_gam_p,
+                             log10_p=vla_log10_p)
 
     def by_ao(flags):
         """Selection function to split by backend flags."""
@@ -142,13 +144,13 @@ else:
     selection_gbt = selections.Selection(by_gbt)
     selection_vla = selections.Selection(by_vla)
 
-    tdgp_ao = gp_signals.BasisGP(ao_qp, dm_basis, name='ao_periodic',
+    tdgp_ao = gp_signals.BasisGP(ao_qp, ao_basis, name='ao_periodic',
                                   coefficients=False,
                                   selection=selection_ao)
-    tdgp_gbt = gp_signals.BasisGP(gbt_qp, dm_basis, name='gbt_periodic',
+    tdgp_gbt = gp_signals.BasisGP(gbt_qp, gbt_basis, name='gbt_periodic',
                                   coefficients=False,
                                   selection=selection_gbt)
-    tdgp_vla = gp_signals.BasisGP(vla_qp, dm_basis, name='vla_periodic',
+    tdgp_vla = gp_signals.BasisGP(vla_qp, vla_basis, name='vla_periodic',
                                   coefficients=False,
                                   selection=selection_vla)
 
