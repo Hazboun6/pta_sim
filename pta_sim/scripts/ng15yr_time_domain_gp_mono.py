4dab2f2fea3e5ce8acaf703f8d35289df8bf5f1d
Hazboun6 <jeffrey.hazboun@gmail.com>
2022-09-23 16:55:03 -0700

    first commit

---
+++ pta_sim/scripts/ng15yr_time_domain_gp_mono.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import numpy as np
+import la_forge.core as co
+import pickle, json, copy, os, sys
+import matplotlib.pyplot as plt
+import cloudpickle
+
+import enterprise
+from enterprise.pulsar import Pulsar
+import enterprise.signals.parameter as parameter
+from enterprise.signals import utils
+from enterprise.signals import signal_base
+from enterprise.signals import selections
+from enterprise.signals.selections import Selection
+from enterprise.signals import white_signals
+from enterprise.signals import gp_signals
+from enterprise.signals import deterministic_signals
+from enterprise import constants as const
+
+from enterprise_extensions.gp_kernels import periodic_kernel
+from enterprise_extensions import sampler
+
+import pta_sim
+import pta_sim.parse_sim as parse_sim
+from pta_sim.bayes import chain_length_bool, save_core, get_freqs, filter_psr_path
+args = parse_sim.arguments()
+
+with open(args.pickle,'rb')as fin:
+    psrs = pickle.load(fin)
+
+if os.path.exists(args.pta_pkl):
+    with open(args.pta_pkl, "rb") as f:
+        pta = cloudpickle.load(f)
+else:
+    tmin = np.amin([p.toas.min() for p in psrs])
+    tmax = np.amax([p.toas.max() for p in psrs])
+    Tspan = tmax - tmin
+
+    # Red noise parameter priors
+    log10_A = parameter.Uniform(-20, -11)
+    gamma = parameter.Uniform(0, 7)
+
+    # GW parameter priors
+    if args.gamma_gw is None:
+        gw_log10_A = parameter.Uniform(-18, -11)('gw_log10_A')
+        gw_gamma = parameter.Uniform(0, 7)('gw_gamma')
+    else:
+        if np.abs(args.gamma_gw - 4.33) < 0.1:
+            gw_log10_A = parameter.Uniform(-18, -14)('gw_log10_A')
+        else:
+            gw_log10_A = parameter.Uniform(-18, -11)('gw_log10_A')
+        gw_gamma = parameter.Constant(args.gamma_gw)('gw_gamma')
+
+    # White noise parameter priors
+    efac = parameter.Constant()
+    equad = parameter.Constant()
+    ecorr = parameter.Constant()
+
+    Nf = args.nfreqs
+    freqs = np.linspace(1/Tspan,Nf/Tspan,Nf)
+
+
+    # # white noise
+    selection = selections.Selection(selections.nanograv_backends)
+
+    ef = white_signals.MeasurementNoise(efac=efac,
+                                        log10_t2equad=equad,
+                                        selection=selection)
+    ec = white_signals.EcorrKernelNoise(log10_ecorr=ecorr, selection=selection)
+
+    # red noise (powerlaw with 30 frequencies)
+    pl = utils.powerlaw(log10_A=log10_A, gamma=gamma)
+    rn = gp_signals.FourierBasisGP(spectrum=pl, modes=freqs)
+
+    # timing model
+    tm = gp_signals.MarginalizingTimingModel()
+
+    log10_sigma = parameter.Uniform(-10, -3)('log10_sigma')
+    log10_ell = parameter.Uniform(1, 5)('log10_ell')
+    log10_p = parameter.Uniform(-2, 2)('log10_p')
+    log10_gam_p = parameter.Uniform(-2, 2)('log10_gam_p')
+
+
+    @signal_base. function
+    def linear_interp_basis_time(toas, dt=7*const.day):
+         # get linear interpolation basis in time
+         U, avetoas = utils.linear_interp_basis(toas, dt=dt)
+
+         return U, avetoas
+
+    qp_basis = linear_interp_basis_time(dt=7*const.day)
+    qp = periodic_kernel(log10_sigma=log10_sigma,
+                            log10_ell=log10_ell,
+                            log10_gam_p=log10_gam_p,
+                            log10_p=log10_p)
+
+
+
+    selection_qp = selections.Selection(selections.by_telescope)
+
+    hdorf = utils.hd_orf
+    monoorf = utils.monopole_orf
+    tdgp = gp_signals.BasisCommonGP2(qp, qp_basis, orf=monoorf, name='mono',
+                                     coefficients=args.gp_coeff,
+                                     selection=selection_qp)
+
+    # gw (powerlaw with 5 frequencies)
+
+    gw_pl = utils.powerlaw(log10_A=gw_log10_A, gamma=gw_gamma)
+    gw = gp_signals.FourierBasisCommonGP(spectrum=gw_pl, orf=hdorf,
+                                         modes=freqs[:args.n_gwbfreqs],
+                                         name='gw')
+
+    model = tm + ef + ec + rn + gw + tdgp
+
+    # models = []
+    # for psr in psrs:
+    #     psr_model = model
+    #     if any([be in psr.flags['be'] for be in ["ASP", "PUPPI"]]):
+    #         psr_model += tdgp_ao
+    #
+    #     if any([be in psr.flags['be'] for be in ["GASP", "GUPPI"]]):
+    #         psr_model += tdgp_gbt
+    #
+    #     if "YUPPI" in psr.flags['be']:
+    #         psr_model += tdgp_vla
+    #
+    #     models.append(psr_model(psr))
+
+    pta = signal_base.PTA([model(p) for p in psrs])
+
+    with open(args.noisepath,'r') as fin:
+        noise = json.load(fin)
+
+    pta.set_default_params(noise)
+
+    if args.mk_ptapkl:
+        with open(args.pta_pkl, "wb") as f:
+            cloudpickle.dump(pta,f)
+
+Sampler = sampler.setup_sampler(pta, outdir=args.outdir, resume=True,
+                                empirical_distr = args.emp_distr)
+def draw_from_gw_gamma_prior(self, x, iter, beta):
+
+    q = x.copy()
+    lqxy = 0
+
+    # draw parameter from signal model
+    signal_name = [par for par in self.pnames
+                   if ('gw' in par and 'gamma' in par)][0]
+    idx = list(self.pnames).index(signal_name)
+    param = self.params[idx]
+
+    q[self.pmap[str(param)]] = np.random.uniform(param.prior._defaults['pmin'], param.prior._defaults['pmax'])
+
+    # forward-backward jump probability
+    lqxy = (param.get_logpdf(x[self.pmap[str(param)]]) -
+            param.get_logpdf(q[self.pmap[str(param)]]))
+
+    return q, float(lqxy)
+
+gp_pars = [par for par in pta.param_names if [s in par for s in ['log10_ell',
+                                                                 'log10_sigma',
+                                                                 'log10_gam_p',
+                                                                 'log10_p']]]
+
+Sampler.addProposalToCycle(Sampler.jp.draw_from_par_prior(gp_pars), 30)
+
+if args.gamma_gw is None:
+    sampler.JumpProposal.draw_from_gw_gamma_prior = draw_from_gw_gamma_prior
+    Sampler.addProposalToCycle(Sampler.jp.draw_from_gw_gamma_prior, 25)
+
+try:
+    achrom_freqs = get_freqs(pta, signal_id='gw_crn')
+    np.savetxt(args.outdir + 'achrom_freqs.txt', achrom_freqs, fmt='%.18e')
+except:
+    pass
+
+x0 = np.hstack(p.sample() for p in pta.params)
+Sampler.sample(x0, args.niter, SCAMweight=200, AMweight=100,
+               DEweight=200, burn=50000, writeHotChains=args.writeHotChains,
+               hotChain=args.hot_chain)
