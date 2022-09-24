3cf1cbeab01084e101318a5db6794b3483f729c2
Hazboun6 <jeffrey.hazboun@gmail.com>
2022-09-24 00:25:36 -0700

    3 bases

--- pta_sim/scripts/ng15yr_time_domain_gp_mono.py
+++ pta_sim/scripts/ng15yr_time_domain_gp_mono.py
@@ -78,35 +78,112 @@ else:
     # timing model
     tm = gp_signals.MarginalizingTimingModel()
 
-    log10_sigma = parameter.Uniform(-10, -3)#('log10_sigma')
-    log10_ell = parameter.Uniform(1, 5)#('log10_ell')
-    log10_p = parameter.Uniform(-2, 2)#('log10_p')
-    log10_gam_p = parameter.Uniform(-2, 2)#('log10_gam_p')
+    # log10_sigma = parameter.Uniform(-10, -3)#('log10_sigma')
+    # log10_ell = parameter.Uniform(1, 5)#('log10_ell')
+    # log10_p = parameter.Uniform(-2, 2)#('log10_p')
+    # log10_gam_p = parameter.Uniform(-2, 2)#('log10_gam_p')
+    #
+    #
+    # @signal_base. function
+    # def linear_interp_basis_time(toas, dt=7*const.day):
+    #      # get linear interpolation basis in time
+    #      U, avetoas = utils.linear_interp_basis(toas, dt=dt)
+    #
+    #      return U, avetoas
+    #
+    # qp_basis = linear_interp_basis_time(dt=7*const.day)
+    # qp = periodic_kernel(log10_sigma=log10_sigma,
+    #                         log10_ell=log10_ell,
+    #                         log10_gam_p=log10_gam_p,
+    #                         log10_p=log10_p)
+    #
+    #
+    #
+    # selection_qp = selections.Selection(selections.by_telescope)
+    #
+    # hdorf = model_orfs.hd_orf()
+    # monoorf = model_orfs.monopole_orf()
+    # tdgp = gp_signals.BasisCommonGP2(qp, qp_basis, monoorf, name='mono',
+    #                                  coefficients=args.gp_coeff,
+    #                                  selection=selection_qp)
+    ao_log10_sigma = parameter.Uniform(-10, -3)('ao_log10_sigma')
+    ao_log10_ell = parameter.Uniform(1, 5)('ao_log10_ell')
+    ao_log10_p = parameter.Uniform(-2, 2)('ao_log10_p')
+    ao_log10_gam_p = parameter.Uniform(-2, 2)('ao_log10_gam_p')
 
+    gbt_log10_sigma = parameter.Uniform(-10, -3)('gbt_log10_sigma')
+    gbt_log10_ell = parameter.Uniform(1, 5)('gbt_log10_ell')
+    gbt_log10_p = parameter.Uniform(-2, 2)('gbt_log10_p')
+    gbt_log10_gam_p = parameter.Uniform(-2, 2)('gbt_log10_gam_p')
+
+    vla_log10_sigma = parameter.Uniform(-10, -3)('vla_log10_sigma')
+    vla_log10_ell = parameter.Uniform(1, 5)('vla_log10_ell')
+    vla_log10_p = parameter.Uniform(-2, 2)('vla_log10_p')
+    vla_log10_gam_p = parameter.Uniform(-2, 2)('vla_log10_gam_p')
 
     @signal_base. function
-    def linear_interp_basis_time(toas, dt=7*const.day):
+    def linear_interp_basis_msk(toas, flags, flagvals=[], dt=7*const.day):
          # get linear interpolation basis in time
-         U, avetoas = utils.linear_interp_basis(toas, dt=dt)
+         # print(name)
+         # print(toas.size)
+         mask = [be in flagvals for be in flags['be']]
+         msk_toas = toas[mask]
+         # print(msk_toas.size)
+         U, avetoas = utils.linear_interp_basis(msk_toas, dt=dt)
 
          return U, avetoas
 
-    qp_basis = linear_interp_basis_time(dt=7*const.day)
-    qp = periodic_kernel(log10_sigma=log10_sigma,
-                            log10_ell=log10_ell,
-                            log10_gam_p=log10_gam_p,
-                            log10_p=log10_p)
+    ao_basis = linear_interp_basis_msk(flagvals=["ASP", "PUPPI"],dt=7*const.day)
+    ao_qp = periodic_kernel(log10_sigma=ao_log10_sigma,
+                            log10_ell=ao_log10_ell,
+                            log10_gam_p=ao_log10_gam_p,
+                            log10_p=ao_log10_p)
+    gbt_basis = linear_interp_basis_msk(flagvals=["GASP", "GUPPI"],dt=7*const.day)
+    gbt_qp = periodic_kernel(log10_sigma=gbt_log10_sigma,
+                             log10_ell=gbt_log10_ell,
+                             log10_gam_p=gbt_log10_gam_p,
+                             log10_p=gbt_log10_p)
+    vla_basis = linear_interp_basis_msk(flagvals=["YUPPI"],dt=7*const.day)
+    vla_qp = periodic_kernel(log10_sigma=vla_log10_sigma,
+                             log10_ell=vla_log10_ell,
+                             log10_gam_p=vla_log10_gam_p,
+                             log10_p=vla_log10_p)
 
+    def by_ao(flags):
+        """Selection function to split by backend flags."""
+        flagvals = ["ASP", "PUPPI"]
+        return {'ao': [be in flagvals for be in flags['be']]}
 
+    def by_gbt(flags):
+        """Selection function to split by backend flags."""
+        flagvals = ["GASP", "GUPPI"]
+        return {'gbt': [be in flagvals for be in flags['be']]}
 
-    selection_qp = selections.Selection(selections.by_telescope)
+    def by_vla(flags):
+        """Selection function to split by backend flags."""
+        flagvals = ["YUPPI"]
+        return {'vla': [be in flagvals for be in flags['be']]}
+
+    selection_ao = selections.Selection(by_ao)
+    selection_gbt = selections.Selection(by_gbt)
+    selection_vla = selections.Selection(by_vla)
 
     hdorf = model_orfs.hd_orf()
     monoorf = model_orfs.monopole_orf()
-    tdgp = gp_signals.BasisCommonGP2(qp, qp_basis, monoorf, name='mono',
-                                     coefficients=args.gp_coeff,
-                                     selection=selection_qp)
 
+    tdgp_ao = gp_signals.BasisCommonGP2(ao_qp, ao_basis, monoorf, name='ao_mono',
+                                 coefficients=args.gp_coeff,
+                                 selection=selection_ao)
+    tdgp_gbt = gp_signals.BasisCommonGP2(gbt_qp, gbt_basis, monoorf, name='gbt_mono',
+                                  coefficients=args.gp_coeff,
+                                  selection=selection_gbt)
+    tdgp_vla = gp_signals.BasisCommonGP2(vla_qp, vla_basis, monoorf, name='vla_mono',
+                                  coefficients=args.gp_coeff,
+                                  selection=selection_vla)
+
+    # tdgp = gp_signals.BasisCommonGP2(qp, qp_basis, monoorf, name='mono',
+    #                                  coefficients=args.gp_coeff,
+    #                                  selection=selection_qp)
     # gw (powerlaw with 5 frequencies)
 
     gw_pl = utils.powerlaw(log10_A=gw_log10_A, gamma=gw_gamma)
@@ -114,21 +191,22 @@ else:
                                          modes=freqs[:args.n_gwbfreqs],
                                          name='gw')
 
-    model = tm + ef + ec + rn + gw + tdgp
+    model = tm + ef + ec + rn + gw
 
-    # models = []
-    # for psr in psrs:
-    #     psr_model = model
-    #     if any([be in psr.flags['be'] for be in ["ASP", "PUPPI"]]):
-    #         psr_model += tdgp_ao
-    #
-    #     if any([be in psr.flags['be'] for be in ["GASP", "GUPPI"]]):
-    #         psr_model += tdgp_gbt
-    #
-    #     if "YUPPI" in psr.flags['be']:
-    #         psr_model += tdgp_vla
-    #
-    #     models.append(psr_model(psr))
+    models = []
+    for psr in psrs:
+        psr_model = model
+        if any([be in psr.flags['be'] for be in ["ASP", "PUPPI"]]):
+            psr_model += tdgp_ao
+
+        if any([be in psr.flags['be'] for be in ["GASP", "GUPPI"]]):
+            psr_model += tdgp_gbt
+
+        if "YUPPI" in psr.flags['be']:
+            psr_model += tdgp_vla
+
+        models.append(psr_model(psr))
+
 
     pta = signal_base.PTA([model(p) for p in psrs])
 
