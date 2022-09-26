0c511e9f8eaffff4a4ee6f5f97bc4f18f5ca8343
Hazboun6 <jeffrey.hazboun@gmail.com>
2022-09-25 22:56:11 -0700

    normal NMOS 2

--- pta_sim/scripts/ng15yr_nm_os.py
+++ pta_sim/scripts/ng15yr_nm_os.py
@@ -37,7 +37,7 @@ else:
     raise ValueError(f'{args.pta_pkl} does not exist.')
 
 
-os_pshift = OS(psrs=psrs, pta=pta, orf=args.orf,
+pta_os = OS(psrs=psrs, pta=pta, orf=args.orf,
                gamma_common=args.gamma_gw)
 
 c0 = co.Core(corepath=args.corepath)
@@ -55,41 +55,13 @@ if 'gw_crn_gamma' in pars:
     pidx = pars.index('gw_crn_gamma')
     pars[pidx] = 'gw_gamma'
 
-N = args.niter
-# M = args.miter
-
-if os.path.exists(args.outdir+f'os_snr_{args.process}.txt'):
-    with open(args.outdir+f'os_snr_{args.process}.txt','r') as file:
-        # get the last line and the start
-        for line in file:
-            pass
-        last_entry = line.split('\t')[-1].split('\n')[0]
-        if 'Pshift Seed' in last_entry:
-            Nstart = 0
-        else:
-            Nstart = int(float(last_entry)) - args.miter*args.process + 1
+if args.orf is "multiple":
+    xi, rho, sig, A, A_err = pta_os.compute_noise_marginalized_multiple_corr_os(chain,param_names=pars)
 else:
-    with open(args.outdir+f'os_snr_{args.process}.txt','w') as file:
-        file.write('\t'.join(['OS (\hat{A}^2)','SNR','Pshift Seed'])+'\n')
-    Nstart = 0
+    xi, rho, sig, A, A_err = pta_os.compute_noise_marginalized_os(chain,param_names=pars)
 
-
-for jj in range(Nstart, N):
-    Ahat_pshift = np.zeros(N)
-    snr_pshift = np.zeros(N)
-    for ii in range(Nstart, N):
-        param_dict = {}
-        if not args.mlv:
-            idx = ii#np.random.randint(0,chain.shape[0])
-        else:
-            idx = mlv_idx
-        param_dict = dict(zip(pars,chain[idx,:]))
-        _, _, _, Asqr, Sigma = os_pshift.compute_os(params=param_dict)
-        Ahat_pshift[ii] = Asqr
-        snr_pshift[ii] = Asqr/Sigma
-        # if ii in check:
-        #     print(f'{ii/N*100} % complete.')
-
-    out = np.array([np.mean(Ahat_pshift),np.mean(snr_pshift)])
-    with open(args.outdir+f'os_snr_{args.process}.txt','a') as file:
-        file.write('\t'.join(out.astype(str))+'\n')
+np.save(args.outdir+f'os_xi_{args.orf}',xi)
+np.save(args.outdir+f'os_rho{args.orf}',rho)
+np.save(args.outdir+f'os_sig_{args.orf}',sig)
+np.save(args.outdir+f'os_A_{args.orf}',A)
+np.save(args.outdir+f'os_A_err_{args.orf}',A_err)
