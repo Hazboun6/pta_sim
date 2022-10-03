670f5685d18d966458ebd7fc4aeff33ff6c74adb
Hazboun6 <jeffrey.hazboun@gmail.com>
2022-10-03 16:07:55 -0700

    excess anm 5

--- pta_sim/scripts/advnoise_excess_noise.py
+++ pta_sim/scripts/advnoise_excess_noise.py
@@ -364,6 +364,8 @@ if args.ladderpath is not None:
 else:
     ladder = None
 
+Sampler.propCycle =  Sampler.propCycle[:len(Sampler.propCycle)-20]
+
 Sampler.sample(x0, args.niter, ladder=ladder, SCAMweight=200, AMweight=100,
                DEweight=200, burn=3000, writeHotChains=args.writeHotChains,
                hotChain=args.hot_chain, Tskip=100, Tmax=args.tempmax)
