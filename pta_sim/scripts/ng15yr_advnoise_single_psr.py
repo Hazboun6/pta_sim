acb2b94f5e1c022379b319a8be7c22dd38767a8b
Jeffrey Hazboun <jeffrey.hazboun@gmail.com>
2023-09-07 09:10:32 -0700

    make DE burn higher

--- pta_sim/scripts/ng15yr_advnoise_single_psr.py
+++ pta_sim/scripts/ng15yr_advnoise_single_psr.py
@@ -368,5 +368,5 @@ else:
     ladder = None
 
 Sampler.sample(x0, args.niter, ladder=ladder, SCAMweight=200, AMweight=100,
-               DEweight=200, burn=3000, writeHotChains=args.writeHotChains,
+               DEweight=200, burn=100000, writeHotChains=args.writeHotChains,
                hotChain=args.hot_chain, Tskip=100, Tmax=args.tempmax)
